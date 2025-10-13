a3074f9109b71021d20afb7f01bbb23d9a4d6dd1
arjunkumar0304 <arjuuxxak04@gmail.com>
2025-10-13 21:34:10 +0530

    data type .py

---
+++ data type .py
@@ -0,0 +1,39 @@
+# Different data types in Python
+
+# Integer
+int_num = 10
+
+# Float
+float_num = 3.14
+
+# Complex
+complex_num = 2 + 3j
+
+# String
+string_val = "Hello, Python!"
+
+# List
+list_val = [1, 2, 3, 4, 5]
+
+# Tuple
+tuple_val = (10, 20, 30)
+
+# Range
+range_val = range(1, 6)
+
+# Dictionary
+dict_val = {"name": "Arjun", "age": 22, "city": "Chennai"}
+
+# Set
+set_val = {1, 2, 3, 4, 5}
+
+# Printing all data types
+print("Integer:", int_num)
+print("Float:", float_num)
+print("Complex:", complex_num)
+print("String:", string_val)
+print("List:", list_val)
+print("Tuple:", tuple_val)
+print("Range:", list(range_val))
+print("Dictionary:", dict_val)
+print("Set:", set_val)
