75dfa7f53bff8f2212ceff737884725634b738ac
arjunkumar0304 <arjuuxxak04@gmail.com>
2025-10-17 17:02:39 +0530

    fun and mod

---
+++ functionandmodule.py/fun.py
@@ -0,0 +1,20 @@
+def add():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Sum:", a + b)
+
+add()
+
+def sub():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Sub:", a - b)
+
+sub()
+
+def mul():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("mul:", a * b)
+
+mul()
---
+++ functionandmodule.py/module.py
@@ -0,0 +1,7 @@
+import math
+
+num = 25
+print("Square root of", num, "is:", math.sqrt(num))
+print("Factorial of 5:", math.factorial(5))
+print("Value of π:", math.pi)
+print("5 raised to 3:", math.pow(5, 3))
